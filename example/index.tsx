ccbb41aa4acd4cf45a25f6b5056e0bec73688f48
Ali Ramshini <ali.ramshini72@gmail.com>
2023-02-27 18:54:12 +0330

    build again

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,14 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {Button} from "../dist";
 
 const App = () => {
   return (
     <div>
+      <Button type={'btn-accent'} >salam</Button>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
